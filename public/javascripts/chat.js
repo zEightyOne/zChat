3a1064df19d62dc735654d442878ae314ad6e72a
capncanuck <cooper.canada@gmail.com>
2017-12-10 12:51:20 -0800

    Completed Chat constructor with prototype functions

---
+++ public/javascripts/chat.js
@@ -0,0 +1,48 @@
+var Chat = function(socket) {
+	this.socket = socket;
+};
+
+/*
+	Is this equivalet to the following:
+
+	function Chat(socket) {
+		this.socket = socket;
+	};
+*/
+
+Chat.prototype.changeRoom = function(room) { 
+	this.socket.emit('join', {
+		newRoom: room
+	});
+};
+
+Chat.prototype.sendMessage = function(room, text) {
+	var message = {
+		room: room,
+		text: text
+	};
+	this.socket.emit('message', message);
+};
+
+Chat.prototype.processCommand = function(command) {
+	var words = command.split(' '); //put the command '/join' or '/nick' in words[0]
+	var command = words[0].substring(1,words[0].length).toLowerCase(); //strip the '/' off and take care of case
+	var message = false;
+
+	switch(command) {
+		case 'join':
+			words.shift(); //shift off 'join' words[0]
+			var room = words.join(' '); //merge the rest of the array into a string (elements seperated by a ' ') - allows for multi-word rooms
+			this.changeRoom(room);
+			break;
+		case 'nick':
+			words.shift();
+			var name = words.join(' '); 
+			this.socket.emit('nameAttempt', name); //will cause function(name) defined in socket.on('nameAttempt', function(name)) to be called on the server
+			break;
+		default:
+			message = 'Unrecognized command.';
+			break;
+	}
+	return message;	
+};
