c588f32edeb80bf732b5dc049ca90384a280dc79
capncanuck <cooper.canada@gmail.com>
2017-12-14 16:51:17 -0800

    Need to fix weird input buffer errror with commands

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -11,108 +11,22 @@ function divSystemContentElement(message) {
 
 
 function processUserInput(chatApp, socket) {
-//	var message = $('#send-message').val() //non-JQuery: document.getElementById('send-message').value
 	var message = vueContent.sendMessage;
 	var systemMessage;
 
 	if (message.charAt(0) == '/') {
 		systemMessage = chatApp.processCommand(message);
 		if (systemMessage) {
-//			$('#messages').append(divSystemContentElement(systemMessage));
 			vueContent.messages.push(systemMessage);
-			// 
-			//non-JQuery: document.getElementById('messages').innerHTML += systemMessage
-			//NEVER do it this way though if you have listeners on these elements instead:
-			//
-			//var content = document.createTextNode(systemMessage)
-			//document.getElementById('messages').appendChild(content)
-			//
 		}
 	}	else {
-//		chatApp.sendMessage($('#room').text(),message); //Broadcast message to the room
 		chatApp.sendMessage(vueContent.room,message);
-		//non-JQuery: chatApp.sendMessage(document.getElementById('send-message').textContent, message);
-//		$('#messages').append(divEscapedContentElement(message)); 
 		vueContent.messages.push(message); //Vue automatically escapes data
 
-		//non-JQuery: var content = document.createTextNode(message); document.getElementById('messages').appendChild(content)
 		$('#messages').scrollTop($('#messages').prop('scrollHeight')); //TODO: Research non-JQuery scrollTop https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
 	}
-//	$('#send-message').val(''); //document.getElementById('send-message').value = '';
 	vueContent.sendMessage = '';
 }
 
-var socket = io.connect();
 
-$(document).ready(function() { //non-JQuery: document.addEventListener("DOMContentLoaded", function(event) { 
-	var chatApp = new Chat(socket) 
-
-/*
-
-	LISTENERS
-
-*/
-
-
-	socket.on('nameResult', function(result) { //LISTENER for when the server calls emit('nameResult',result)
-		var message;
-
-		if(result.success) {
-			message = 'You are now known as ' + result.name + '.'; //eg. result = {success: true, name: 'Bob'}
-		} else {
-			message = result.message;
-		}
-//		$('#messages').append(divSystemContentElement(message)); //non-JQuery: var content = document.createTextNode(message); document.getElementById('messages').appendChild(content) 
-		vueContent.messages.push(message);
-	});
-
-
-	socket.on('joinResult', function(result){ //LISTENER for when the server calls emit('joinResult',result)
-		$('#room').text(result.room); //non-JQuery document.getElementById('room').textContent = room
-//		$('#messages').append(divSystemContentElement('Room changed.'));
-		vueContent.messages.push('Room changed.');		
-		//non-JQuery: var content = document.createTextNode('Room changed.'); document.getElementById('messages').appendChild(content)
-	});
-
-	socket.on('message', function(message) { //LISTENER for when the server calls emit('message',message)
-//		var newElement = $('<div></div>').text(message.text); //TODO: why not call var newElement = divEscapedContentElement(message.text)
-//		$('#messages').append(newElement); //non-JQuery: document.getElementById('messages').appendChild(newElement) 
-		vueContent.messages.push(message.text);		
-	});
-
-	socket.on('rooms', function(rooms) { //LISTENER for when the server calls emit('rooms', rooms)
-		numberOfCalls++;
-//		$('#room-list').empty(); //Not sure why this is being used as this is not a list
-		vueContent.roomList = [];
-		for(var room in rooms) { //Display list of rooms
-			room = room.substring(1,room.length);
-			if (room != '') {
-//				$('#room-list').append(divEscapedContentElement(room));
-				vueContent.roomList.push(room);
-			}
-		}
-
-/*
-
-	END LISTENERS
-
-*/
-
-		$('#room-list div').click(function() { //EVENT HANDLER: ONCLICK
-			chatApp.processCommand('/join ' + $(this).text() ); //Join room when you click on it
-			$('#send-message').focus();
-		});
-	});
-
-	setInterval(function() { //Reqest list of room every 1000 ms
-		socket.emit('rooms');
-	}, 1000); 
-
-	$('#send-message').focus();
-
-	$('#send-form').submit(function() { //EVENT HANDLER: ONSUBMIT
-		processUserInput(chatApp, socket);
-		return false;
-	});
-});
 
--- public/javascripts/chat_vue.js
+++ public/javascripts/chat_vue.js
@@ -1,12 +1,73 @@
+
+
 var vueContent = new Vue( {
 	el: '#vue-content',
 	data: {
 		appName: '<a href="/">Growler</a>',
-		appLogo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_vT47OPPq_oH-ZkwtxMZ7C33WQBbK8MRFKscKyzTe48Yj3IUM',
+		appLogo: '/img/logo.jpg',
 		isThirsty: true,
 		sendMessage: '',
 		messages: [],
 		roomList: [],
 		room: ''
-	}
-});
+	},
+	methods: {
+		init: function(){
+			var socket = io.connect();
+			var chatApp = new Chat(socket); 
+
+			//EVENT HANDLERS
+
+			socket.on('nameResult', function(result) { //LISTENER for when the server calls emit('nameResult',result)
+				var message;
+				if(result.success) {
+					message = 'You are now known as ' + result.name + '.'; //eg. result = {success: true, name: 'Bob'}
+				} else {
+					message = result.message;
+				}
+				vueContent.messages.push(message);
+			});
+
+			socket.on('joinResult', function(result){ //LISTENER for when the server calls emit('joinResult',result)
+				$('#room').text(result.room); //non-JQuery document.getElementById('room').textContent = room
+				vueContent.messages.push('Room changed.');		
+			});
+
+			socket.on('message', function(message) { //LISTENER for when the server calls emit('message',message)
+				vueContent.messages.push(message.text);		
+			});
+
+			socket.on('rooms', function(rooms) { //LISTENER for when the server calls emit('rooms', rooms)
+				numberOfCalls++;
+				vueContent.roomList = [];
+				for(var room in rooms) { //Display list of rooms
+					room = room.substring(1,room.length);
+					if (room != '') {
+						vueContent.roomList.push(room);
+					}
+				}
+
+			//END-EVENT HANDLERS
+
+			$('#room-list div').click(function() { //EVENT HANDLER: ONCLICK
+					chatApp.processCommand('/join ' + $(this).text() ); //Join room when you click on it
+					$('#send-message').focus();
+				});
+			});
+
+			setInterval(function() { //Reqest list of room every 1000 ms
+				socket.emit('rooms');
+			}, 1000); 
+
+			$('#send-message').focus();
+
+			$('#send-form').submit(function() { //EVENT HANDLER: ONSUBMIT
+				processUserInput(chatApp, socket);
+				return false;
+			});
+		}
+	},	
+	mounted: function(){
+			this.init();
+			}
+	});
