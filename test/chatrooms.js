ab16379232a6a25d314bf797af12c14eb16e058b
mark081 <cooper.canada@gmail.com>
2018-03-05 16:56:47 -0800

    added mocha unit test

--- test/chatrooms.js
+++ test/chatrooms.js
@@ -1,49 +1,24 @@
 const chatrooms = require('../lib/chat_server');
+const io = require('socket.io-client');
 const chai = require('chai');
 const assert = chai.assert;
 chai.should(); //changes prototypes
 const expect = chai.expect;
-
-
-
+let socket;
 
 describe('chatrooms', () => {
-    beforeEach(() => {
-        chatrooms.clear();
-    });
-    describe('.saveSync(doc)', () => {
-        it('should save the doc', () => {
-            const pet = {name: 'Tobi'};
-            chatrooms.saveSync(pet);
-            const ret = chatrooms.first({name: 'Tobi'});
-            assert(ret === pet);
-        });
-    });
-    describe('.first(obj)', () => {
-        it('first should return ther first matching doc', () => {
-            const pet1 = {name: 'Tobi'};
-            const pet2 = {name: 'Pobi'};
-            chatrooms.saveSync(pet1);
-            chatrooms.saveSync(pet2);
-            let ret = chatrooms.first({name: 'Tobi'});
-            assert.deepEqual(ret, pet1);
-            ret = chatrooms.first({name: 'Pobi'});
-            assert.deepEqual(ret,pet2);
-        });
-        it('first should return null if no matching doc', () => {
-            ret = chatrooms.first({name: 'Robi'});
-            expect(ret).to.not.equal({name: 'Robi'});
-        })
-    });
+    before(() => socket = io.connect('http://localhost:3000'));
+    describe('Name validation', () => {
+        it('assignGuestName', () => {
+            let number = 1000;
+            let result = chatrooms.assignGuestName(socket,number,[],[]);
 
-    describe('.save(doc,cb)', () => {
-        it('should save the doc', (done) => {
-            const doc = {name: 'Tobi'};
-            chatrooms.save(doc, () => {
-                let ret = chatrooms.first({name: 'Tobi'});
-                ret.name.should.equal('Tobi');
-                done();
-            });
+            result.should.equal(number+1);
         });
     });
+    describe('TODO', () => {
+        it('TODO', () => {
+        });
+    });
+    after( () => socket.disconnect());
 });
